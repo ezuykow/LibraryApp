2f63afc7b07d4fd46480c5757089200e12bd5a3c
ezuykow <zuy1996@gmail.com>
2022-11-03 18:25:45 +0300

    all done

--- src/main/java/ru/ezuykow/app/controller/BookController.java
+++ src/main/java/ru/ezuykow/app/controller/BookController.java
@@ -4,24 +4,26 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 import ru.ezuykow.app.dao.BookDAO;
+import ru.ezuykow.app.dao.PersonDAO;
 import ru.ezuykow.app.model.Book;
+import ru.ezuykow.app.model.Person;
 
 import javax.validation.Valid;
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/books")
 public class BookController {
 
     private BookDAO bookDAO;
+    private PersonDAO personDAO;
 
     @Autowired
-    public BookController(BookDAO bookDAO) {
+    public BookController(BookDAO bookDAO, PersonDAO personDAO) {
         this.bookDAO = bookDAO;
+        this.personDAO = personDAO;
     }
 
     @GetMapping()
@@ -42,6 +44,56 @@ public class BookController {
             return "/books/new";
         }
         bookDAO.add(book);
+        return "redirect:/books";
+    }
+
+    @GetMapping("/{book_id}")
+    public String bookPage(@PathVariable("book_id") int book_id, Model model,
+                           @ModelAttribute("person") Person person) {
+        model.addAttribute("book", bookDAO.getBookById(book_id));
+
+        Optional<Person> owner = bookDAO.getBookOwner(book_id);
+        if (owner.isPresent()) {
+            model.addAttribute("owner", owner.get());
+        } else {
+            model.addAttribute("people", personDAO.index());
+        }
+
+        return "/books/book";
+    }
+
+    @PatchMapping("/{book_id}/assign")
+    public String assignBook(@PathVariable("book_id") int book_id, @ModelAttribute("person") Person person) {
+        bookDAO.assign(book_id, person);
+        return "redirect:/books/" + book_id;
+    }
+
+    @PatchMapping("/{book_id}/release")
+    public String releaseBook(@PathVariable("book_id") int book_id) {
+        bookDAO.release(book_id);
+        return "redirect:/books/" + book_id;
+    }
+
+    @GetMapping("/{book_id}/edit")
+    public String editPage(@PathVariable("book_id") int book_id, Model model) {
+        model.addAttribute("book", bookDAO.getBookById(book_id));
+        return "/books/edit";
+    }
+
+    @PatchMapping("/{book_id}")
+    public String editBook(@PathVariable("book_id") int book_id,
+                           @Valid @ModelAttribute("book") Book book,
+                           BindingResult bindingResult) {
+        if (bindingResult.hasErrors()) {
+            return "books/edit";
+        }
+        bookDAO.edit(book_id, book);
+        return "redirect:/books";
+    }
+
+    @DeleteMapping("/{book_id}")
+    public String deleteBook(@PathVariable("book_id") int book_id) {
+        bookDAO.delete(book_id);
         return "redirect:/books";
     }
 }
--- src/main/java/ru/ezuykow/app/dao/BookDAO.java
+++ src/main/java/ru/ezuykow/app/dao/BookDAO.java
@@ -6,8 +6,10 @@ import org.springframework.jdbc.core.BeanPropertyRowMapper;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 import ru.ezuykow.app.model.Book;
+import ru.ezuykow.app.model.Person;
 
 import java.util.List;
+import java.util.Optional;
 
 @Component
 public class BookDAO {
@@ -37,5 +39,32 @@ public class BookDAO {
                         "FROM person JOIN book ON person.person_id = ? AND book.person_id=?",
                 new Object[]{person_id, person_id},
                 new BeanPropertyRowMapper<>(Book.class));
+    }
+
+    public Book getBookById(int book_id) {
+        return jdbcTemplate.query("SELECT * FROM book WHERE book_id=?",
+                new BeanPropertyRowMapper<>(Book.class), book_id).stream().findAny().orElse(null);
+    }
+
+    public Optional<Person> getBookOwner(int book_id) {
+        return jdbcTemplate.query("SELECT person.* FROM person JOIN book ON book.person_id = person.person_id" +
+                        " WHERE book_id=?", new BeanPropertyRowMapper<>(Person.class), book_id).stream().findAny();
+    }
+
+    public void assign(int book_id, Person person) {
+        jdbcTemplate.update("UPDATE book SET person_id=? WHERE book_id=?", person.getPerson_id(), book_id);
+    }
+
+    public void release(int book_id) {
+        jdbcTemplate.update("UPDATE book SET person_id=NULL WHERE book_id=?", book_id);
+    }
+
+    public void edit(int book_id, Book book) {
+        jdbcTemplate.update("UPDATE book SET title=?, author=?, publishingYear=? WHERE book_id=?",
+                book.getTitle(), book.getAuthor(), book.getPublishingYear(), book.getBook_id());
+    }
+
+    public void delete(int book_id) {
+        jdbcTemplate.update("DELETE FROM book WHERE book_id=?", book_id);
     }
 }
